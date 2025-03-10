d4edf102d34a38cf7abeb22d4e985054bce505f0
ToNga156 <ngaichi2004@gmail.com>
2025-03-11 06:33:31 +0700

    buoi5

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
-use App\Http\Controllers\SignUpController;
+use App\Http\Controllers\CreateTableController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 
-Route::get('/', function () {
-    return view('form');
+Route::get('/create', [CreateTableController::class, 'createTable']);
+Route::get('/create-db', function (){
+    Schema::create('products', function ($table) {
+        $table->increments('id');
+        $table->string('name');
+        $table->float('price');
+        $table->string('image');
+    });
 });
-Route::post('/', [SignUpController::class, 'signUp']);
